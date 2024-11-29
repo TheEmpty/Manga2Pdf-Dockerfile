aec3e7031bf3b0352d005e19576f6a8439d4a7f7
Myles Best <myles+git@empty.best>
2024-11-29 09:28:56 -0500

    Fix PDF generation
    OCR folder needed a little more crafting. And mokuro2pdf needs the ttf file in the repo.

--- run.sh
+++ run.sh
@@ -9,7 +9,10 @@ function manga2pdf() {
   echo "Starting on ${1}"
   echo '---'
   /app/bin/mokuro "${1}" --disable_confirmation --unzip --legacy-html
-  ruby /app/mokuro2pdf/Mokuro2Pdf.rb -i "${1}" -o "${1}/../_ocr" -w "/out"
+  FOLDER="$(basename "${1}")"
+  PARENT_DIR="$(dirname "${1}")"
+  cd /app/mokuro2pdf # required for ttf
+  ruby /app/mokuro2pdf/Mokuro2Pdf.rb -u -i "${1}" -o "${PARENT_DIR}/_ocr/${FOLDER}" -w "/out"
   echo '---'
   echo "Finshed on ${1}"
   echo '+++'
